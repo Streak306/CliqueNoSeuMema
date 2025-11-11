20561824eb9e274d4811c9c14392b7746fd9e5f1
duduzw <duducraft52@gmail.com>
2025-11-11 16:55:14 -0300

    Update data.js

--- docs/js/data.js
+++ docs/js/data.js
@@ -63,7 +63,7 @@ export const UPGRADE_DATA = [
     name:'Mão de Graphite',
     effect:'O click e os dedos são duas vezes tão eficientes.',
     cost:500,
-<img src="imagens/melhorias-futuras/up-dedo-t2.png" draggable="false" alt="Mão de Graphite">
+    <img src="imagens/melhorias-futuras/up-dedo-t2.png" draggable="false" alt="Mão de Graphite">
     requirement:{type:'building', building:'dedo', count:1},
     requirementText:'Possuir 1 dedo',
     requires:['up-dedo-t1'],
